befa40f18f98b12ed093238185b211ddb535d858
Natallia Zakharenkava <lady.natashka2010@yandex.ru>
2023-01-28 12:19:51 +0300

    task3 done

--- 24.01/3/Program.cs
+++ 24.01/3/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = new int[8];
+
+void FillArray(int[] curt) {
+    int lenght = curt.Length;
+    int i = 0;
+    while (i < lenght)  {
+        curt[i] = new Random().Next(1, 8);
+        i++;
+    }
+};
+
+void PrintArray(int[] cobein) {
+    int count = cobein.Length;
+    int position = 0;
+    while (position < count) {
+        System.Console.WriteLine(cobein[position]);
+        position++;
+    }
+}
+
+FillArray(array);
+PrintArray(array);

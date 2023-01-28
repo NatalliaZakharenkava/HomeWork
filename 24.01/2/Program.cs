b5c18f36389967493926e2dc1c4750d8640fed78
Natallia Zakharenkava <lady.natashka2010@yandex.ru>
2023-01-28 11:20:52 +0300

    task2 doneф

--- 24.01/2/Program.cs
+++ 24.01/2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("--> ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+   int Sum(int n, int sum) 
+   {  while (n > 0) {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+    };
+
+  sum = Sum(n, sum);
+
+System.Console.WriteLine("сумма " + sum);
+

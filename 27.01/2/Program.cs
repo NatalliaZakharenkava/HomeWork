c8091cdcd212e8ea29419c3af669b4bd29d882ae
Natallia Zakharenkava <lady.natashka2010@yandex.ru>
2023-01-29 15:01:55 +0300

    task2 done

---
+++ 27.01/2/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = new int[6];
+Random rnd = new Random();
+int Sum = 0;
+for (int i = 0; i < array.Length; i++) {
+    array[i] = rnd.Next(1, 20);
+    System.Console.Write(" " + array[i]);
+    if (i % 2 == 1) {
+    Sum = array[i] + Sum;
+};
+
+};
+
+System.Console.WriteLine(" Сумма элементов нв нечетных позициях " +  Sum);

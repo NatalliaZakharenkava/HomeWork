0a16890c564aaba9c082aadf337f10a9e70edfed
Natallia Zakharenkava <lady.natashka2010@yandex.ru>
2023-01-29 15:57:39 +0300

    task3 done

---
+++ 27.01/3/Program.cs
@@ -0,0 +1,28 @@
+﻿double[] array = new double[5];
+Random rnd = new Random();
+
+
+
+for (int i = 0; i < array.Length; i++) {
+    array[i] = rnd.Next(1, 10);
+    System.Console.Write(array[i] + " ");
+  };
+
+double max = array[0];
+double min = array[0];
+
+for (int i = 0; i < array.Length; i++) {
+    if (max < array[i]) {
+        max = array[i];
+    }
+    if (min > array[i]) {
+        min = array[i];
+    }
+};
+
+double MaxMin = max - min;
+
+System.Console.WriteLine(" \nРазница между максимальным и минимальным числом " + MaxMin);
+
+
+

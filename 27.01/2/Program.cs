fdf96b83967c747d1741dcb05c32b408b8152250
Natallia Zakharenkava <lady.natashka2010@yandex.ru>
2023-02-06 10:50:10 +0300

    Задача 1

--- 27.01/2/Program.cs
+++ 27.01/2/Program.cs
@@ -4,10 +4,10 @@ int Sum = 0;
 for (int i = 0; i < array.Length; i++) {
     array[i] = rnd.Next(1, 20);
     System.Console.Write(" " + array[i]);
-    if (i % 2 == 1) {
+    if (i % 2 != 0) {
     Sum = array[i] + Sum;
 };
 
 };
 
-System.Console.WriteLine(" Сумма элементов нв нечетных позициях " +  Sum);
+System.Console.WriteLine(" Сумма элементов на нечетных позициях " +  Sum);

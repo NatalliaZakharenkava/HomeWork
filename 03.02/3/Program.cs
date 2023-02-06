6ac739ccff5e3842332e1e6087b0b88395639306
Natallia Zakharenkava <lady.natashka2010@yandex.ru>
2023-02-06 14:40:37 +0300

    задача 3

---
+++ 03.02/3/Program.cs
@@ -0,0 +1,45 @@
+﻿int[,] numbers = new int[3, 4];
+
+FillArray(numbers);
+
+
+for (int j = 0; j < numbers.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        avarage = (avarage + numbers[i, j]);
+    }
+    avarage = avarage / 3;
+    Console.Write(Math.Round(avarage, 1) + "; ");
+}
+Console.WriteLine();
+PrintArray(numbers);
+
+
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        
+        Console.WriteLine(" ");
+    }
+}

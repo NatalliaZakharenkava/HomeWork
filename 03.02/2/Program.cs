d686c730a6776e61ef05a1c798392a6ccef1889d
Natallia Zakharenkava <lady.natashka2010@yandex.ru>
2023-02-06 14:02:08 +0300

    задача 2

---
+++ 03.02/2/Program.cs
@@ -0,0 +1,78 @@
+﻿int m = 4;
+int n = 4;
+
+int[,] array = new int[m, n];
+
+Random rnd = new Random();
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    System.Console.WriteLine(" ");
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = rnd.Next(10);
+        System.Console.Write(array[i, j] + " ");
+
+    }
+}
+System.Console.WriteLine(" ");
+
+System.Console.WriteLine("Введите позиции элемента, разделяя запятой: ");
+
+string? positionElement = Console.ReadLine();
+positionElement = RemovingSpaces(positionElement);
+int[] position = ParserString(positionElement);
+
+if(position[0] <= m 
+&& position[1] <= n 
+&& position[0] >= 0 
+&& position[1] >= 0) 
+{
+  int result = array[position[0]-1, position[1]-1];
+  Console.Write($"Значение элемента: {result}");
+}
+else Console.Write($"такого элемента в массиве нет.");
+
+int[] ParserString(string input)
+{
+  int countNumbers = 1;
+  for (int i = 0; i < input.Length; i++)
+  {
+      if (input[i] == ',')
+          countNumbers++;
+  }
+
+  int[] numbers = new int[countNumbers];
+
+  int numberIndex = 0;
+  for(int i = 0; i < input.Length; i++)
+  {
+    string subString = String.Empty;
+
+    while (input[i] != ',')
+    {
+      subString += input[i].ToString();
+      if (i >= input.Length - 1)
+        break;
+      i++;
+    }
+    numbers[numberIndex] = Convert.ToInt32(subString);
+    numberIndex++;
+  }
+
+  return numbers;
+}
+
+string RemovingSpaces (string input)
+{
+  string output = String.Empty;
+  for (int i = 0; i < input.Length; i++)
+  {
+    if (input[i] != ' ') 
+    {
+      output += input[i];
+    }
+  }
+  return output;
+}
+

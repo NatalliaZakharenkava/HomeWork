fde2be3d77736d5a3e8e6ae22cab7fa594315861
Natallia Zakharenkava <lady.natashka2010@yandex.ru>
2023-02-10 12:32:47 +0300

    task 1

---
+++ 07.02/1/Program.cs
@@ -0,0 +1,48 @@
+﻿int[,] array = new int[3, 4];
+
+RandomNumbers(array);
+System.Console.WriteLine();
+PrintArray(array);
+
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1) - 1; j++)
+    {
+        for (int l = 0; l < array.GetLength(1) - 1; l++)
+        {
+            if (array[i, l] < array[i, l + 1]) {
+                int buf = 0;
+                buf = array[i, l];
+                array[i, l] = array[i, l + 1];
+                array[i, l + 1] = buf;
+            }
+        }
+    }
+}
+
+System.Console.WriteLine();
+PrintArray(array);
+
+void RandomNumbers (int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine("");
+    }
+}

a758ea2265a540840d397221c30bf9e1098f7ee3
Natallia Zakharenkava <lady.natashka2010@yandex.ru>
2023-02-11 13:32:35 +0300

    task 1

---
+++ 10.02/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задача 64: Задайте значение N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии. */
+
+System.Console.WriteLine("--> ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+
+string Function (int a, int n)
+{
+    
+    if (a <= n) return Function(a + 1, n) + $"{a} ";
+    else return String.Empty;
+}
+System.Console.WriteLine(Function(a, n));

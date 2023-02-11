6eeddf8992106142fbaf4a6640f9bb905edcdc43
Natallia Zakharenkava <lady.natashka2010@yandex.ru>
2023-02-11 14:20:58 +0300

    task 3

--- 10.02/Program.cs
+++ 10.02/Program.cs
@@ -1,8 +1,8 @@
-﻿/* Задача 64: Задайте значение N. Напишите программу, 
+﻿/*  Задача 64: Задайте значение N. Напишите программу, 
 которая выведет все натуральные числа в промежутке от N до 1. 
-Выполнить с помощью рекурсии. */
+Выполнить с помощью рекурсии.  */
 
-/* System.Console.WriteLine("--> ");
+ System.Console.WriteLine("--> ");
 
 int n = Convert.ToInt32(Console.ReadLine());
 int a = 1;
@@ -13,12 +13,12 @@ string Function (int a, int n)
     if (a <= n) return Function(a + 1, n) + $"{a} ";
     else return String.Empty;
 }
-System.Console.WriteLine(Function(a, n)); */
+System.Console.WriteLine(Function(a, n)); 
 
 /* Задача 66: Задайте значения M и N. 
 Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. */
 
-System.Console.WriteLine("Введите первое число: ");
+/* System.Console.WriteLine("Введите первое число: ");
 
 int m = Convert.ToInt32(Console.ReadLine());
 
@@ -31,4 +31,21 @@ int ResultSum(int m, int n)
    if (m == n) return n;
    else return n + ResultSum(m, n - 1);
 }
-System.Console.WriteLine(ResultSum(m, n));
+System.Console.WriteLine(ResultSum(m, n)); */
+
+/* Задача 68: Напишите программу вычисления функции Аккермана
+с помощью рекурсии. Даны два неотрицательных числа m и n. */
+
+
+/*      System.Console.WriteLine("Введите первое число: ");
+     int m = Convert.ToInt32(Console.ReadLine());
+     System.Console.WriteLine("Введите второе число: ");
+     int n = Convert.ToInt32(Console.ReadLine());
+
+     int Function(int m, int n)
+     {
+        if (n == 0) return Function(m - 1, 1);
+        else if (m == 0) return n + 1;
+        else return Function(m - 1, Function(m, n - 1));
+     }
+     System.Console.WriteLine(Function(m, n)); */
